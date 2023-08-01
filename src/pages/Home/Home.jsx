dc8298f03138c09f7b76c1452d477c7a606f8820
muteenk <kundangarmuteen@gmail.com>
2023-08-01 13:14:34 +0530

    Updated Hero and Nav

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import './Home.css'
 
 // Image imports
-import heroImg from '../../assets/home/home-hero.jpeg'
+import heroImg from '../../assets/home/home-hero.png'
 
 const Home = () => {
   return (
@@ -11,12 +12,17 @@ const Home = () => {
 
       <section id="home-hero">
 
-        <div className="hero-info">
-          <h1 className='hero-heading'>Feel the lightning speed softwares with <span className='accent-text'>GRENUD's top notch professionals</span></h1>
+        <div className="hero-display">
+          <div className="header">
+            <a href="#"><i class="fa-solid fa-bars"></i></a>
+          </div>
+          <img src={heroImg} alt="" />
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officiis harum sint perferendis optio dignissimos consequuntur. Facilis eaque dolore maiores ad?</p>
+          <Link className="pri-btn" href="#">Know More</Link>
         </div>
 
-        <div className="hero-display">
-          <img src={heroImg} alt="" />
+        <div className="hero-info">
+          <h1 className='hero-heading'>Feel the lightning speed softwares with GRENUD's top notch professionals</h1>
         </div>
 
       </section>
